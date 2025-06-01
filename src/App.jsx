4d77bd5ccb863c67b3ae59802d6e973a15eb7473
Seva <20211206nch@mail.ru>
2025-06-01 11:42:27 +0300

    changed structure in React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import List from '@/components/List'
+import { Route, Routes } from 'react-router-dom'
+
+import ArticlesPage from '@/pages/ArticlesPage'
+import OneArticlePage from '@/pages/OneArticlePage'
+import RegPage from '@/pages/RegPage'
+import NotFoundPage from '@/pages/NotFoundPage'
+import EditForm from '@/pages/EditForm'
+import Layout from '@/models/Layout'
 
 function App() {
   return (
-    <div className="relative container mx-auto">
-      <List />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ArticlesPage />} />
+        <Route path="articles" element={<ArticlesPage />} />
+        <Route path="articles/:slug" element={<OneArticlePage />} />
+        <Route path="sign_up" element={<RegPage />} />
+        <Route path="sign_in" element={<RegPage />} />
+        <Route path="create_article" element={<EditForm />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/models/Layout/Layout.jsx
+++ src/models/Layout/Layout.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import Header from '@/models/Header'
 
 function Layout() {
-  return <div>Layout</div>
+  return (
+    <>
+      <Header />
+
+      <main className="container mx-auto mt-24">
+        <Outlet />
+      </main>
+    </>
+  )
 }
 
 export default Layout
