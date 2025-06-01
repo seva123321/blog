7fbfc1ad1089d7701b7644e1129b8abadd679892
Seva <20211206nch@mail.ru>
2025-06-01 11:39:59 +0300

    modified articleApi

--- src/redux/articleApi.js
+++ src/redux/articleApi.js
@@ -1,4 +1,7 @@
+/* eslint-disable indent */
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import { PAGINATION_LIMIT } from '@/services/utils'
 
 import BASE_URL from '../config'
 
@@ -8,9 +11,49 @@ const articleApi = createApi({
     baseUrl: `${BASE_URL}/articles`,
   }),
   endpoints: (build) => ({
+    // getArticles: build.query({
+    //   query: ({ offset = 0, limit = 20 }) => ({
+    //     url: '',
+    //     params: { offset, limit },
+    //   }),
+    // }),
     getArticles: build.query({
-      query: () => '',
+      query: ({ offset = 0, limit = PAGINATION_LIMIT }) => ({
+        url: '',
+        params: { offset, limit },
+      }),
+      // eslint-disable-next-line arrow-body-style
+      providesTags: (result) => {
+        return result
+          ? [
+              ...result.articles.map(({ slug }) => ({
+                type: 'Articles',
+                id: slug,
+              })),
+              { type: 'Articles', id: 'PARTIAL-LIST' },
+            ]
+          : [{ type: 'Articles', id: 'PARTIAL-LIST' }]
+      },
+      // Автоматически объединяем данные для пагинации
+      serializeQueryArgs: ({ queryArgs }) => ({ offset: queryArgs.offset }),
+      merge: (currentCache, newItems, { arg }) => {
+        if (arg.offset === 0 || !currentCache) return newItems
+
+        return {
+          ...newItems,
+          articles: [...newItems.articles],
+        }
+      },
+      // Определяем когда нужно перезаписать данные
+      forceRefetch: ({ currentArg, previousArg }) =>
+        // eslint-disable-next-line implicit-arrow-linebreak
+        currentArg?.offset === previousArg?.offset,
     }),
+
+    // https://blog-platform.kata.academy/api/articles?offset=20&limit=20
+    // getArticlesOffset: build.query({
+    //   query: ({ offset = 0, limit = 20 }) => `?offset=${offset}&limit=${limit}`,
+    // }),
     getArticleBySlug: build.query({
       query: (slug) => `/${slug}`,
     }),
@@ -20,14 +63,39 @@ const articleApi = createApi({
         method: 'POST',
         body,
       }),
+      invalidatesTags: (result, error, slug) => [
+        { type: 'Articles', slug },
+        { type: 'Articles', slug: 'PARTIAL-LIST' },
+      ],
     }),
   }),
 })
-
 export const {
   useGetArticlesQuery,
   useGetArticleBySlugQuery,
   usePostArticleMutation,
+  useLazyGetArticlesQuery,
+  useGetArticlesOffsetQuery,
 } = articleApi
 
 export default articleApi
+
+// export const articleApi = createApi({
+//   reducerPath: 'articleApi',
+//   baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/articles` }),
+//   endpoints: (builder) => ({
+//     getArticles: builder.query({
+//       query: ({ offset = 0, limit = 20 }) => ({
+//         url: '',
+//         params: { offset, limit }
+//       }),
+//       // Автоматически объединяем данные для пагинации
+//       serializeQueryArgs: ({ endpointName }) => endpointName,
+//       merge: (currentCache, newItems) => {
+//         currentCache.articles.push(...newItems.articles)
+//       },
+//       // Определяем когда нужно перезаписать данные
+//       forceRefetch: ({ currentArg, previousArg }) => currentArg?.offset === 0
+//     })
+//   })
+// })
