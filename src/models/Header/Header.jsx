339133a2a63049ea87e6352b749bfc35ae1abd93
Seva <20211206nch@mail.ru>
2025-06-02 21:28:08 +0300

    create routes.js config and add in App/Header/NotFoundPage

--- src/models/Header/Header.jsx
+++ src/models/Header/Header.jsx
@@ -1,22 +1,25 @@
 import { Link } from 'react-router-dom'
 
 import AvaComponent from '@/components/AvaComponent'
+import ROUTES from '@/services/routes'
 
 function Header() {
   return (
-    <div className="fixed top-0 right-0 left-0 z-10 mb-6 flex h-20 items-center justify-between bg-white px-6 text-[18px] shadow">
+    <header className="fixed top-0 right-0 left-0 z-10 mb-6 flex h-20 items-center justify-between bg-white px-6 text-[18px] shadow">
       <div className="">Realworld Blog</div>
 
       <div className="flex items-center gap-7">
         <div className="">
           <Link
-            to="create_article"
+            to={ROUTES.CREATE_ARTICLE}
             className="cursor-pointer rounded-md border-2 border-[#52C41A] px-3 py-1.5 whitespace-nowrap text-[#52C41A] transition-colors duration-300 hover:bg-[#53c41a2d]"
           >
             Create article
           </Link>
         </div>
-        <AvaComponent username="ИМЯ" urlImg="/icons/FallbackAvatar.png" />
+        <Link to={ROUTES.PROFILE}>
+          <AvaComponent username="ИМЯ" urlImg="/icons/FallbackAvatar.png" />
+        </Link>
 
         <div className="">
           <button
@@ -26,20 +29,20 @@ function Header() {
             Log Out
           </button>
           <Link
-            to="sign_up"
+            to={ROUTES.SIGN_UP}
             className="cursor-pointer rounded-md border-2 border-[#52C41A] px-4 py-2.5 whitespace-nowrap text-[#52C41A] transition-colors duration-300 hover:bg-[#53c41a2d]"
           >
             Sign Up
           </Link>
           <Link
-            to="sign_in"
+            to={ROUTES.SIGN_IN}
             className="cursor-pointer rounded-md border-2 border-black px-4 py-2.5 whitespace-nowrap text-black transition-colors duration-300 hover:bg-[#4d504c2d]"
           >
             Sign In
           </Link>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
 
---
+++ src/services/routes.js
@@ -0,0 +1,12 @@
+const ROUTES = {
+  HOME: '/',
+  SIGN_UP: '/sign-up',
+  SIGN_IN: '/sign-in',
+  ARTICLES: '/articles',
+  ARTICLE: '/articles/:slug',
+  CREATE_ARTICLE: '/create-article',
+  PROFILE: '/profile',
+  // EDIT_ARTICLE: '/edit_article/:slug?',
+}
+
+export default ROUTES
