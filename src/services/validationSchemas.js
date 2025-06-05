cc197d509abd6e1694e519aba1128ccd222abd0f
Seva <20211206nch@mail.ru>
2025-06-05 07:01:42 +0300

    create validationShema yup

--- src/services/validationSchemas.js
+++ src/services/validationSchemas.js
@@ -1,12 +1,19 @@
 import * as yup from 'yup'
 
-const RegExpEmail = /^\S+@\S+\.\S+$/
+const RegExpEmail = /(^[a-z][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/
+// const RegExpEmail = /^\S+@\S+\.\S+$/
+const RegExpUser = /^[a-z][a-z0-9]*$/
 
 export const SignUpSchema = yup.object().shape({
+  // 'You can only use lowercase English letters and numbers',
   username: yup
     .string()
     .trim()
     .required('This field is required!')
+    .matches(
+      RegExpUser,
+      'You can only use lowercase English letters and numbers',
+    )
     .min(3, 'Minimum 3 characters required')
     .max(25, 'Maximum length is 25 characters'),
   email: yup
@@ -28,4 +35,14 @@ export const SignUpSchema = yup.object().shape({
     .required('You must agree to continue'),
 })
 
-export const SignInSchema = yup.object().shape({})
+export const SignInSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required('This field is required!')
+    .matches(RegExpEmail, 'Invalid email format'),
+  password: yup
+    .string()
+    .required('This field is required!')
+    .min(6, 'Minimum 6 characters required')
+    .max(40, 'Maximum length is 40 characters'),
+})
