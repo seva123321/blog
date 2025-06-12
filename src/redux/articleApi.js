45c39a795411ac77799acc3e3ae6c9de4b12c914
Seva <20211206nch@mail.ru>
2025-06-12 14:52:12 +0300

    add LikedArticle/UnLikedArticle in articleApi

--- src/redux/articleApi.js
+++ src/redux/articleApi.js
@@ -87,6 +87,26 @@ const articleApi = createApi({
         { type: 'Article', id: 'LIST' },
       ],
     }),
+    likedArticle: build.mutation({
+      query: (slug) => ({
+        url: `/${slug}/favorite`,
+        method: 'POST',
+      }),
+      invalidatesTags: (result, error, { slug }) => [
+        { type: 'Article', id: slug },
+        { type: 'Article', id: 'LIST' },
+      ],
+    }),
+    unLikedArticle: build.mutation({
+      query: (slug) => ({
+        url: `/${slug}/favorite`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, { slug }) => [
+        { type: 'Article', id: slug },
+        { type: 'Article', id: 'LIST' },
+      ],
+    }),
     deleteArticle: build.mutation({
       query: (slug) => ({
         url: `/${slug}`,
@@ -101,116 +121,10 @@ export const {
   useGetArticleBySlugQuery,
   usePostArticleMutation,
   useUpdateArticleMutation,
+  useLikedArticleMutation,
+  useUnLikedArticleMutation,
   useDeleteArticleMutation,
   useLazyGetArticlesQuery,
-  useGetArticlesOffsetQuery,
 } = articleApi
 
 export default articleApi
-// /* eslint-disable indent */
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-// import { PAGINATION_LIMIT } from '@/services/utils'
-// import Encryption from '@/services/encryption'
-
-// import BASE_URL from '../config'
-
-// const articleApi = createApi({
-//   reducerPath: 'articleApi',
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: `${BASE_URL}/articles`,
-//     prepareHeaders: async (headers) => {
-//       const userData = localStorage.getItem('user')
-//       if (!userData) return headers
-
-//       try {
-//         const cryptoService = new Encryption()
-//         const userDecrypted = await cryptoService.decrypt(JSON.parse(userData))
-//         headers.set('Content-Type', 'application/json')
-
-//         if (userDecrypted?.token) {
-//           headers.set('Authorization', `Token ${userDecrypted.token}`)
-//         }
-
-//         return headers
-//       } catch (error) {
-//         return headers
-//       }
-//     },
-//   }),
-//   tagTypes: ['Article'],
-//   endpoints: (build) => ({
-//     getArticles: build.query({
-//       query: ({ offset = 0, limit = PAGINATION_LIMIT }) => ({
-//         url: '',
-//         params: { offset, limit },
-//       }),
-//       // eslint-disable-next-line arrow-body-style
-//       providesTags: (result) => {
-//         return result
-//           ? [
-//               ...result.articles.map(({ slug }) => ({
-//                 type: 'Articles',
-//                 id: slug,
-//               })),
-//               { type: 'Articles', id: 'PARTIAL-LIST' },
-//             ]
-//           : [{ type: 'Articles', id: 'PARTIAL-LIST' }]
-//       },
-//       // Автоматически объединяем данные для пагинации
-//       serializeQueryArgs: ({ queryArgs }) => ({ offset: queryArgs.offset }),
-//       merge: (currentCache, newItems, { arg }) => {
-//         if (arg.offset === 0 || !currentCache) return newItems
-
-//         return {
-//           ...newItems,
-//           articles: [...newItems.articles],
-//         }
-//       },
-//       // Определяем когда нужно перезаписать данные
-//       forceRefetch: ({ currentArg, previousArg }) =>
-//         // eslint-disable-next-line implicit-arrow-linebreak
-//         currentArg?.offset === previousArg?.offset,
-//     }),
-
-//     getArticleBySlug: build.query({
-//       query: (slug) => `/${slug}`,
-//     }),
-//     postArticle: build.mutation({
-//       query: (body) => ({
-//         url: '',
-//         method: 'POST',
-//         body: { article: { ...body } },
-//       }),
-//       // invalidatesTags: ['Articles'],
-//       // providesTags: ['Articles'],
-//     }),
-//     updateArticle: build.mutation({
-//       query: ({ body, slug }) => ({
-//         url: `/${slug}`,
-//         method: 'PUT',
-//         body: { article: { ...body } },
-//       }),
-//       invalidatesTags: ['Articles'],
-//       // providesTags: ['Articles'],
-//     }),
-//     deleteArticle: build.mutation({
-//       query: (slug) => ({
-//         url: `/${slug}`,
-//         method: 'DELETE',
-//       }),
-//       invalidatesTags: ['Articles'],
-//     }),
-//   }),
-// })
-// export const {
-//   useGetArticlesQuery,
-//   useGetArticleBySlugQuery,
-//   usePostArticleMutation,
-//   useUpdateArticleMutation,
-//   useDeleteArticleMutation,
-//   useLazyGetArticlesQuery,
-//   useGetArticlesOffsetQuery,
-// } = articleApi
-
-// export default articleApi
