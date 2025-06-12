ef0dac2cacdf145524d597048655e274b448dd4a
Seva <20211206nch@mail.ru>
2025-06-12 14:42:20 +0300

    create ProfileSchema in validationSchemas

--- src/services/validationSchemas.js
+++ src/services/validationSchemas.js
@@ -52,3 +52,38 @@ export const EditFormSchema = yup.object().shape({
   description: yup.string().required('This field is required!'),
   body: yup.string().required('This field is required!'),
 })
+
+export const ProfileSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required('This field is required!')
+    .matches(
+      RegExpUser,
+      'You can only use lowercase English letters and numbers',
+    )
+    .min(3, 'Minimum 3 characters required')
+    .max(25, 'Maximum length is 25 characters'),
+  email: yup
+    .string()
+    .required('This field is required!')
+    .matches(RegExpEmail, 'Invalid email format'),
+  password: yup
+    .string()
+    .required('This field is required!')
+    .min(6, 'Minimum 6 characters required')
+    .max(40, 'Maximum length is 40 characters'),
+  image: yup
+    .string()
+    .url('Должен быть корректным URL')
+    .test('is-valid-image-url', 'URL должен вести на изображение', (value) => {
+      if (!value) return true
+
+      const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.webp']
+      return imageExtensions.some((ext) => value.toLowerCase().endsWith(ext))
+    })
+    .test('is-valid-url', 'URL должен использовать HTTPS', (value) => {
+      if (!value) return true
+      return value.startsWith('https://')
+    }),
+})
